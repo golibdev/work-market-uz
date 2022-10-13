31d5eb9ccb9422579f0712914b649cece6c987b0
golibdev <gnarzullayev2000@gmail.com>
2022-10-13 10:42:47 +0500

    fix error

--- server/src/routes/employerRoutes.js
+++ server/src/routes/employerRoutes.js
@@ -1,11 +1,13 @@
-const router = require('express').Router();
+const { Router } = require('express');
+const router = Router();
 const { employerController } = require('../controllers');
-const { verifyAdmin, verifyEmployer } = require('../middlewares/');
+const { verifyAdmin, verifyEmployer } = require('../middlewares');
 
 router.post('/register', employerController.register);
 router.post('/login', employerController.login);
+router.post('/create-job', verifyEmployer.verifyEmployerToken, employerController.createNewJob);
 router.get('/', verifyAdmin.verifyAdminToken, employerController.getAll);
 router.get('/:id', verifyAdmin.verifyAdminToken, employerController.getOne);
-router.put('/update-password/:id', verifyEmployer.verifyEmployerToken, employerController.updatePassword);
+router.put('/update-password', verifyEmployer.verifyEmployerToken, employerController.updatePassword);
 
 module.exports = router
